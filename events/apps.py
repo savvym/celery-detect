e21610ed3ff94005760ea1cd48ca68299d24ad9c
zhhdzhang <zhhdzhang@tencent.com>
2024-12-20 19:04:34 +0800

    [WIP] 1. 创建celery监听器模块，监听事件，通过channels推送到websocket客户端 2. channels消费来自celery的事件

--- events/apps.py
+++ events/apps.py
@@ -3,7 +3,10 @@
 import logging
 from django.apps import AppConfig
 import threading
+from celery_detect.celery_app import get_celery_app
 from events.celery_events import start_event_listener
+from events.receiver import CeleryEventReceiver
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +19,7 @@ class EventsConfig(AppConfig):
         在 Django 启动时，启动 Celery 事件监听器
         """
         # 使用线程异步启动事件监听器
-        listener_thread = threading.Thread(target=start_event_listener)
-        listener_thread.daemon = True  # 设置为守护线程
-        listener_thread.start()
+        app = get_celery_app()
+        consumer = CeleryEventReceiver(app)
+
+        consumer.start()
--- events/receiver.py
+++ events/receiver.py
@@ -4,9 +4,7 @@ from threading import Event, Thread
 from celery import Celery
 from celery.events import EventReceiver
 from celery.events.state import State
-from channels.layers import get_channel_layer
 from django.conf import settings
-from asyncio import Queue
 
 logger = logging.getLogger(__name__)
 
@@ -23,9 +21,7 @@ class CeleryEventReceiver(Thread):
         super().__init__()
         self.app = app
         self._stop_signal = Event()
-        self.queue = Queue()
         self.receiver: EventReceiver | None = None
-        self.channel_layer = get_channel_layer()  # 获取Django Channels的ChannelLayer
 
     def run(self) -> None:
         """线程运行方法，用于捕获和处理 Celery 事件"""
@@ -54,34 +50,12 @@ class CeleryEventReceiver(Thread):
             logger.info("Starting to consume events...")
             self.receiver.capture(limit=None, timeout=None, wakeup=True)
 
-    async def on_event(self, event: dict) -> None:
+    def on_event(self, event: dict) -> None:
         """处理接收到的 Celery 事件"""
-        logger.debug(f"Received event: {event}")
+        logger.info(f"Received event: {event}")
         state.event(event)  # 更新 Celery 状态
-        self.queue.put_nowait(event)  # 将事件放入队列中
-        await self.broadcast_event(event)  # 将事件广播到 Channels
-
-        if self._stop_signal.is_set():
-            raise KeyboardInterrupt("Stop signal received")
-
-    async def broadcast_event(self, event: dict):
-        """通过 Django Channels 将事件广播到 WebSocket"""
-        try:
-            # 发送事件到所有连接的消费者
-            await self.channel_layer.group_send(
-                "events_group",  # 事件广播的组名
-                {
-                    "type": "broadcast_message",  # 消息类型
-                    "message": event,  # 消息内容
-                }
-            )
-        except Exception as e:
-            logger.exception(f"Failed to broadcast event: {e}")
-
-    def stop(self) -> None:
-        """停止事件消费线程"""
-        logger.info("Stopping event consumer...")
-        if self.receiver is not None:
-            self.receiver.should_stop = True
-        self._stop_signal.set()
-        self.join()
+        # self.queue.put_nowait(event)  # 将事件放入队列中
+        # await self.broadcast_event(event)  # 将事件广播到 Channels
+        #
+        # if self._stop_signal.is_set():
+        #     raise KeyboardInterrupt("Stop signal received")
